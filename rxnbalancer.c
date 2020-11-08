9aa2790c59a67a075041181470f0555b443b0a3e
Brenton Bongcaron <brenton3926@gmail.com>
2020-11-08 16:33:00 -0500

    Read chemical equation from command line, count numReactants. --> replaced with = until redirection nullification issue is addressed.

--- rxnbalancer.c
+++ rxnbalancer.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
+int main(int argc, char* argv[argc+1])
 {
-  printf("testing");
+  int index = 1;
+  int numReactants = 0;
+
+  while (strcmp(argv[index], "=") != 0 && index < (argc + 1))
+  {
+    if (strcmp(argv[index], "+") == 0)
+    {
+      index++;
+      continue;
+    }
+    printf("%s\n", argv[index]);
+    numReactants++;
+    index++;
+  }
+
+  printf("numReactants = %d\n", numReactants);
+
   return 0;
 }
