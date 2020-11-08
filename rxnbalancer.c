5e6882c65df187d16eea8622d7e357890cf6d30b
Brenton Bongcaron <brenton3926@gmail.com>
2020-11-08 18:59:04 -0500

    Moved code that gets reactants from main to its own function: getReactants. Description of getReactants function is added above main.

--- rxnbalancer.c
+++ rxnbalancer.c
@@ -19,6 +19,16 @@ void printComponents(char** components, int numComponents);
 void freeMatrix(char** matrix, int numElements);
 
 /****************************************************************************
+  @desc   Fetch reactants in an equation
+  @author Brenton Bongcaron brenton3926@gmail.com
+  @param  char** equation       --> array containing each "word" in chemical
+                                    equation
+          int argc              --> number of "words" in chemical equation
+          int* numReactants     --> pointer to number of reactants in equation
+*****************************************************************************/
+char** getReactants(char** equation, int argc, int* numReactants);
+
+/****************************************************************************
   @file   rxnbalancer.c
   @desc   This function will take a chemical equation(s) without stoichiometric
           coefficients and balance it.
@@ -30,50 +40,58 @@ void freeMatrix(char** matrix, int numElements);
 *****************************************************************************/
 int main(int argc, char* argv[argc+1])
 {
-  int index = 1;
   int numReactants = 0;
+  char** reactants = getReactants(argv, argc, &numReactants);
 
-  /* Count the number of reactants in the equation */
-  while (strcmp(argv[index], "=") != 0 && index < (argc + 1))
-  {
-    if (strcmp(argv[index], "+") == 0)
-    {
-      index++;
-      continue;
-    }
-    numReactants++;
-    index++;
-  }
-  /*
-    Opted for dyanmic allocation of array instead of linked list because it is
-     preferred to have instant access to all components in the equation at a
-     given moment.
-  */
-
-  index = 1;
-  int reactantIndex = 0;
-  // the array of reactants
-  char** reactants = malloc(sizeof(char*) * numReactants);
-
-  /* Insert each reactant into char** reactants */
-  while (strcmp(argv[index], "=") != 0 && index < (argc + 1))
-  {
-    if (strcmp(argv[index], "+") == 0)
-    {
-      index++;
-      continue;
-    }
-    reactants[reactantIndex] = malloc(sizeof(char) * strlen(argv[index]));
-    strcpy(reactants[reactantIndex], argv[index]);
-    reactantIndex++;
-    index++;
-  }
-  // printComponents(reactants, numReactants);
+  //printf("numReactants = %d\n", numReactants);
+  printComponents(reactants, numReactants);
   // Free the malloc'ed reactants matrix
   freeMatrix(reactants, numReactants);
   return 0;
 }
 
+char** getReactants(char** equation, int argc, int* numReactants)
+{
+  int index = 1;
+  int countReactants = 0;
+
+  /* Count the number of reactants in the equation */
+  while (strcmp(equation[index], "=") != 0 && index < (argc + 1))
+  {
+    if (strcmp(equation[index], "+") == 0)
+    {
+      index++;
+      continue;
+    }
+    countReactants++;
+    index++;
+  }
+  *numReactants = countReactants;
+  /*
+    Opted for dyanmic allocation of array instead of linked list because it is
+     preferred to have instant access to all components in the equation at a
+     given moment.
+  */
+  index = 1;
+  int reactantIndex = 0;
+  // the array of reactants
+  char** reactants = malloc(sizeof(char*) * countReactants);
+
+  /* Insert each reactant into char** reactants */
+  while (strcmp(equation[index], "=") != 0 && index < (argc + 1))
+  {
+    if (strcmp(equation[index], "+") == 0)
+    {
+      index++;
+      continue;
+    }
+    reactants[reactantIndex] = malloc(sizeof(char) * strlen(equation[index]));
+    strcpy(reactants[reactantIndex], equation[index]);
+    reactantIndex++;
+    index++;
+  }
+  return reactants;
+}
 void printComponents(char** components, int numComponents) {
     for (int i = 0; i < numComponents; i++)
     {
