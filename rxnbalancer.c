1463910cc63bf8df6acfecebb7cfdbf7b194c7d9
Brenton Bongcaron <brenton3926@gmail.com>
2020-11-08 17:12:35 -0500

    Added a file header comment and annotated the code in previous commit

--- rxnbalancer.c
+++ rxnbalancer.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+/****************************************************************************
+  @file   rxnbalancer.c
+  @desc   This function will take a chemical equation(s) without stoichiometric
+          coefficients and balance it.
+  @author Brenton Bongcaron brenton3926@gmail.com
+  @param  a chemical equation without stoichiometric coefficents, using =
+          instead of the standard arrow (-->) in the form of a string OR a
+          file containing multiple chemical equations in the above format.
+  @return balanced chemical equation(s)
+*****************************************************************************/
 int main(int argc, char* argv[argc+1])
 {
   int index = 1;
   int numReactants = 0;
 
+  /* Count the number of reactants in the equation */
   while (strcmp(argv[index], "=") != 0 && index < (argc + 1))
   {
     if (strcmp(argv[index], "+") == 0)
@@ -14,12 +24,17 @@ int main(int argc, char* argv[argc+1])
       index++;
       continue;
     }
-    printf("%s\n", argv[index]);
+    //printf("%s\n", argv[index]);
     numReactants++;
     index++;
   }
+  //printf("numReactants = %d\n", numReactants);
 
-  printf("numReactants = %d\n", numReactants);
+  /*
+    Opted for dyanmic allocation of array instead of linked list because it is
+     preferred to have instant access to all components in the equation at a
+     given moment.
+  */
 
   return 0;
 }
