f5856891d51a40ac0f699ae3fb9542a29f8a451b
Brenton Bongcaron <brenton3926@gmail.com>
2020-11-08 17:54:31 -0500

    Added while loop that inserts each reactant string into an array containing the reactant strings. Created a printComponents function for debugging purposes to print an array of components. Created a freeMatrix function to free any malloc'ed matrices.

--- rxnbalancer.c
+++ rxnbalancer.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+/****************************************************************************
+  @desc   Print each string in an array of strings
+  @author Brenton Bongcaron brenton3926@gmail.com
+  @param  char** components     --> array of pointers to strings
+          int numComponents     --> number of pointers to strings in the array
+*****************************************************************************/
+void printComponents(char** components, int numComponents);
+
+/****************************************************************************
+  @desc   Free a malloc'ed array of char strings
+  @author Brenton Bongcaron brenton3926@gmail.com
+  @param  char** matrix         --> malloc'ed matrix to be freed
+          int numElements       --> number of pointers to arrays in the array
+*****************************************************************************/
+void freeMatrix(char** matrix, int numElements);
+
 /****************************************************************************
   @file   rxnbalancer.c
   @desc   This function will take a chemical equation(s) without stoichiometric
@@ -24,17 +41,52 @@ int main(int argc, char* argv[argc+1])
       index++;
       continue;
     }
-    //printf("%s\n", argv[index]);
     numReactants++;
     index++;
   }
-  //printf("numReactants = %d\n", numReactants);
-
   /*
     Opted for dyanmic allocation of array instead of linked list because it is
      preferred to have instant access to all components in the equation at a
      given moment.
   */
 
+  index = 1;
+  int reactantIndex = 0;
+  // the array of reactants
+  char** reactants = malloc(sizeof(char*) * numReactants);
+
+  /* Insert each reactant into char** reactants */
+  while (strcmp(argv[index], "=") != 0 && index < (argc + 1))
+  {
+    if (strcmp(argv[index], "+") == 0)
+    {
+      index++;
+      continue;
+    }
+    reactants[reactantIndex] = malloc(sizeof(char) * strlen(argv[index]));
+    strcpy(reactants[reactantIndex], argv[index]);
+    reactantIndex++;
+    index++;
+  }
+  // printComponents(reactants, numReactants);
+  // Free the malloc'ed reactants matrix
+  freeMatrix(reactants, numReactants);
   return 0;
+}
+
+void printComponents(char** components, int numComponents) {
+    for (int i = 0; i < numComponents; i++)
+    {
+      printf("Component %d: %s\n", i, components[i]);
+    }
+    return;
+}
+
+void freeMatrix(char** matrix, int numElements){
+  for (int i = 0; i < numElements; i++)
+  {
+    free(matrix[i]);
+  }
+  free(matrix);
+  return;
 }
