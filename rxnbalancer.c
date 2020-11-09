1c6c3880ab84ad7d9cb236ee17d727a5a34c37b0
Brenton Bongcaron <brenton3926@gmail.com>
2020-11-08 20:46:16 -0500

    Added getProducts function & getProducts comment header. Tweaked getReactants comments/line spacing.

--- rxnbalancer.c
+++ rxnbalancer.c
@@ -19,7 +19,7 @@ void printComponents(char** components, int numComponents);
 void freeMatrix(char** matrix, int numElements);
 
 /****************************************************************************
-  @desc   Fetch reactants in an equation
+  @desc   Fetch REACTANTS in an equation
   @author Brenton Bongcaron brenton3926@gmail.com
   @param  char** equation       --> array containing each "word" in chemical
                                     equation
@@ -28,6 +28,17 @@ void freeMatrix(char** matrix, int numElements);
   @return char** containing the strings of each reactant in equation
 *****************************************************************************/
 char** getReactants(char** equation, int argc, int* numReactants);
+
+/****************************************************************************
+  @desc   Fetch PRODUCTS in an equation
+  @author Brenton Bongcaron brenton3926@gmail.com
+  @param  char** equation       --> array containing each "word" in chemical
+                                    equation
+          int argc              --> number of "words" in chemical equation
+          int* numProducts      --> pointer to number of products in equation
+  @return char** containing the strings of each product in equation
+*****************************************************************************/
+char** getProducts(char** equation, int argc, int* numProducts);
 
 /****************************************************************************
   @file   rxnbalancer.c
@@ -43,10 +54,20 @@ int main(int argc, char* argv[argc+1])
 {
   int numReactants = 0;
   char** reactants = getReactants(argv, argc, &numReactants);
+
   //printf("numReactants = %d\n", numReactants);
-  printComponents(reactants, numReactants);
-  // Free the malloc'ed reactants matrix
+  //printComponents(reactants, numReactants);
+
+  int numProducts = 0;
+  char** products = getProducts(argv, argc, &numProducts);
+
+  //printf("numProducts = %d\n", numProducts);
+  //printComponents(products, numProducts);
+
+  /* Free the malloc'ed component matrices */
   freeMatrix(reactants, numReactants);
+  freeMatrix(products, numProducts);
+  
   return 0;
 }
 
@@ -65,14 +86,13 @@ char** getReactants(char** equation, int argc, int* numReactants)
     index++;
   }
   *numReactants = countReactants;
+  index = 1;
+  int reactantIndex = 0;
   /*
     Opted for dyanmic allocation of array instead of linked list because it is
      preferred to have instant access to all components in the equation at a
      given moment.
   */
-  index = 1;
-  int reactantIndex = 0;
-  // the array of reactants
   char** reactants = malloc(sizeof(char*) * countReactants);
 
   /* Insert each reactant into char** reactants */
@@ -89,7 +109,60 @@ char** getReactants(char** equation, int argc, int* numReactants)
   return reactants;
 }
 
-void printComponents(char** components, int numComponents) {
+char** getProducts(char** equation, int argc, int* numProducts)
+{
+  int index = 1;
+  int countProducts = 0;
+  int startOfProducts = 0;
+  bool onProductSide = false;
+
+  /* Count the number of products in the equation */
+  while (index < argc)
+  {
+    if (!onProductSide)
+    {
+      if (strcmp(equation[index], "=") == 0)
+      {
+        onProductSide = true;
+        /* mark the index of the first product */
+        startOfProducts = index + 1;
+      }
+    }
+    else
+    {
+      if (strcmp(equation[index], "+") != 0)
+      {
+        countProducts++;
+      }
+    }
+    index++;
+  }
+  *numProducts = countProducts;
+  int productIndex = 0;
+  index = startOfProducts;
+  /*
+    Opted for dyanmic allocation of array instead of linked list because it is
+     preferred to have instant access to all components in the equation at a
+     given moment.
+  */
+  char** products = malloc(sizeof(char*) * countProducts);
+
+  /* Insert each product into char** products */
+  while (index < argc)
+  {
+    if (strcmp(equation[index], "+") != 0)
+    {
+      products[productIndex] = malloc(sizeof(char) * strlen(equation[index]));
+      strcpy(products[productIndex], equation[index]);
+      productIndex++;
+    }
+    index++;
+  }
+  return products;
+}
+
+void printComponents(char** components, int numComponents)
+{
     for (int i = 0; i < numComponents; i++)
     {
       printf("Component %d: %s\n", i + 1, components[i]);
@@ -97,7 +170,8 @@ void printComponents(char** components, int numComponents) {
     return;
 }
 
-void freeMatrix(char** matrix, int numElements){
+void freeMatrix(char** matrix, int numElements)
+{
   for (int i = 0; i < numElements; i++)
   {
     free(matrix[i]);
